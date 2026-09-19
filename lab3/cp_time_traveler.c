1b331fc6f8da42ac0578cd15281c78c688347b35
Tejas <tejas@TMBA.local>
2026-09-19 10:55:41 +0530

    added check for leap years

--- lab3/cp_time_traveler.c
+++ lab3/cp_time_traveler.c
@@ -7,11 +7,14 @@ int main()
     scanf("%i %i %i", &day, &mon, &yer);
     if (day < 32 && mon < 12 && yer >= 0)
     {
-        if (mon == 2 && day < 28)
+        if (mon == 2 && day < 29)
         {
 
         }
-        if (mon == 2)
+        else if (mon == 2 && day == 29 && (yer % 4 == 0 && (yer % 100 != 0 || (yer % 100 == 0 && yer % 400 == 0))))
+        {
+
+        }
     }
     else 
     {
