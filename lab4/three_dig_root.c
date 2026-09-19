8508b39c66727bb62c885c6c3d86469038225a18
Tejas <tejas@TMBA.local>
2026-09-19 16:12:52 +0530

    check for intermediate root and digital root

--- lab4/three_dig_root.c
+++ lab4/three_dig_root.c
@@ -5,5 +5,25 @@ int main()
     int num;
     printf("Enter a number: ");
     scanf("%i", &num);
-    
+    int temp = num;
+    int sum = 0;
+    while (1 == 1)
+    {
+        sum += temp % 10;
+        temp /= 10;
+        if (temp == 0)
+        {
+            printf("Intermediate sum: %i\n", sum);
+            if (sum < 10)
+            {
+                printf("Digital root: %i\n", sum);
+                break;
+            }
+            else
+            {
+                temp = sum;
+                sum = 0;
+            }
+        }
+    }
 }
