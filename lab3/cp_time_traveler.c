9e0205c33aeb9c332ff0e2207f074279d60005f1
Tejas <tejas@TMBA.local>
2026-09-19 11:23:38 +0530

    Fixed typo in next day and dealt with feb exception in months less than 7

--- lab3/cp_time_traveler.c
+++ lab3/cp_time_traveler.c
@@ -9,47 +9,47 @@ int main()
     {
         if (mon == 2 && day < 28)
         {
-            printf("Next day: %i/%i%i\n", day + 1, mon, yer);
+            printf("Next day: %i/%i/%i\n", day + 1, mon, yer);
         }
         else if (mon == 2 && day == 29 && (yer % 4 == 0 && (yer % 100 != 0 || (yer % 100 == 0 && yer % 400 == 0))))
         {
-            printf("Next day: 1/%i%i\n", mon + 1, yer);
+            printf("Next day: %i/%i/%i\n", 1, mon + 1, yer);
         }
         else if (mon == 2 && day == 28 && (yer % 4 == 0 && (yer % 100 != 0 || (yer % 100 == 0 && yer % 400 == 0))))
         {
-            printf("Next day: %i/%i%i\n", day + 1, mon + 1, yer);
+            printf("Next day: %i/%i/%i\n", day + 1, mon + 1, yer);
         }
-        else if (mon < 8 && mon % 2 != 0 && day < 31)
+        else if (mon < 8 && mon % 2 != 0 && day < 31 && mon != 2)
         {
-            printf("Next day: %i/%i%i\n", day + 1, mon, yer);
+            printf("Next day: %i/%i/%i\n", day + 1, mon, yer);
         }
-        else if (mon < 8 && mon % 2 != 0 && day == 31)
+        else if (mon < 8 && mon % 2 != 0 && day == 31 && mon != 2)
         {
-            printf("Next day: %i/%i%i\n", 1, mon + 1, yer);
+            printf("Next day: %i/%i/%i\n", 1, mon + 1, yer);
         }
-        else if (mon < 8 && mon % 2 == 0 && day < 30)
+        else if (mon < 8 && mon % 2 == 0 && day < 30 && mon != 2)
         {
-            printf("Next day: %i/%i%i\n", day + 1, mon, yer);
+            printf("Next day: %i/%i/%i\n", day + 1, mon, yer);
         }
-        else if (mon < 8 && mon % 2 == 0 && day == 30)
+        else if (mon < 8 && mon % 2 == 0 && day == 30 && mon != 2)
         {
-            printf("Next day: %i/%i%i\n", 1, mon + 1, yer);
+            printf("Next day: %i/%i/%i\n", 1, mon + 1, yer);
         }
         else if (mon > 7 && mon % 2 == 0 && day < 31 && mon != 12)
         {
-            printf("Next day: %i/%i%i\n", day + 1, mon, yer);
+            printf("Next day: %i/%i/%i\n", day + 1, mon, yer);
         }
         else if (mon > 7 && mon % 2 == 0 && day == 31 && mon != 12)
         {
-            printf("Next day: %i/%i%i\n", 1, mon + 1, yer);
+            printf("Next day: %i/%i/%i\n", 1, mon + 1, yer);
         }
         else if (mon > 7 && mon % 2 != 0 && day < 30 && mon != 12)
         {
-            printf("Next day: %i/%i%i\n", day + 1, mon, yer);
+            printf("Next day: %i/%i/%i\n", day + 1, mon, yer);
         }
         else if (mon > 7 && mon % 2 != 0 && day == 30 && mon != 12)
         {
-            printf("Next day: %i/%i%i\n", 1, mon + 1, yer);
+            printf("Next day: %i/%i/%i\n", 1, mon + 1, yer);
         }
         else if (mon == 12 && day < 31)
         {
