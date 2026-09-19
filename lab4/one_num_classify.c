75726a2f0f17ebcc9e816e2c63f82d392e9f6a69
Tejas <tejas@TMBA.local>
2026-09-19 11:45:32 +0530

    output the sum of digits and reversed number

--- lab4/one_num_classify.c
+++ lab4/one_num_classify.c
@@ -3,14 +3,24 @@
 int main()
 {
     int num;
-    printf("Enter a number: ");
-    scanf("%i", &num);
     int temp = num;
     int dgts = 0;
+    int sum;
+    int rev = 0;
+    printf("Enter a number: ");
+    scanf("%i", &num);
     while (temp != 0)
     {
         temp /= 10;
         dgts++;
     }
     printf("Digits: %i\n", dgts);
+    while (num != 0)
+    {
+        sum += num % 10;
+        rev = rev * 10 + num % 10;
+        num /= 10;
+    }
+    printf("Sum: %i\n", sum);
+    printf("Reversed: %i\n", rev);
 }
