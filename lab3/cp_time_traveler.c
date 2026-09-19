91c3a3e01b64a26c2bfa073c2be7add94b55f7ab
Tejas <tejas@TMBA.local>
2026-09-19 11:31:03 +0530

    fixed fringe feb bugs

--- lab3/cp_time_traveler.c
+++ lab3/cp_time_traveler.c
@@ -5,7 +5,7 @@ int main()
     int day, mon, yer;
     printf("Input: ");
     scanf("%i %i %i", &day, &mon, &yer);
-    if (day < 32 && mon < 12 && yer >= 0 && day > 0 && mon > 0)
+    if (day < 32 && mon < 13 && yer >= 0 && day > 0 && mon > 0)
     {
         if (mon == 2 && day < 28)
         {
@@ -17,7 +17,11 @@ int main()
         }
         else if (mon == 2 && day == 28 && (yer % 4 == 0 && (yer % 100 != 0 || (yer % 100 == 0 && yer % 400 == 0))))
         {
-            printf("Next day: %i/%i/%i\n", day + 1, mon + 1, yer);
+            printf("Next day: %i/%i/%i\n", 29, mon, yer);
+        }
+        else if (mon == 2 && day == 28)
+        {
+            printf("Next day: %i/%i/%i\n", 1, mon + 1, yer);
         }
         else if (mon < 8 && mon % 2 != 0 && day < 31 && mon != 2)
         {
